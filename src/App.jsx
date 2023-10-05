5ca24e7434ac46b725e3c5a7e5ef8d683f536905
facup14 <facundo@inft.com.ar>
2023-10-05 12:41:48 -0300

    entrega1

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import Navbar from "./components/Navbar"
+import ItemListContainer from "./components/ItemListContainer"
 
 const App = () => {
   return (
     <>
     
+      <Navbar />
+      <ItemListContainer ofertaSemanal="Esta semana la oferta es ..." />
     
     </>
   )
---
+++ src/components/ItemListContainer.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Oferta(props) {
+  return <h1> {props.ofertaSemanal} </h1>
+}
+
+export default Oferta

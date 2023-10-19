89d73f8a9407c4e3b2f13a77c82f0bfb9e48351f
facup14 <facundo@inft.com.ar>
2023-10-19 11:17:37 -0300

    cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Navbar from "./components/Navbar"
 import ItemListContainer from "./components/ItemListContainer"
+import Cards from "./components/Cards"
 
 const App = () => {
   return (
@@ -7,6 +8,7 @@ const App = () => {
     
       <Navbar />
       <ItemListContainer ofertaSemanal="Esta semana la oferta es ..." />
+      <Cards />
     
     </>
   )

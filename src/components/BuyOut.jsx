9a5b6aaf7a5dcac56d3b181f409e7fbc97b2e4bf
facup14dev <facup28@outlook.com>
2023-11-15 20:28:10 -0300

    v 0.21

--- src/components/BuyOut.jsx
+++ src/components/BuyOut.jsx
@@ -1,8 +1,76 @@
 import React from 'react'
+import { useState } from 'react'
+import { collection, addDoc, getFirestore } from 'firebase/firestore'
+import './BuyOut.css'
+
 
 const BuyOut = () => {
+
+  const [nombre, setNombre] = useState('')
+  const [email, setEmail] = useState('')
+  const [orderId, setOrderId] = useState(null)
+  const [showForm, setShowForm] = useState(true);
+
+  const db = getFirestore()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    addDoc(ordersCollection, order).then(({ id }) =>
+      setOrderId(id))
+    setShowForm(false)
+  }
+
+  const order = {
+    nombre,
+    email
+  }
+
+  const ordersCollection = collection(db, 'Orden')
+
+  console.log(showForm)
+
   return (
-    <h1>BuyOut</h1>
+
+    <div>
+
+
+      {showForm && (
+
+        <form action="" onSubmit={handleSubmit} className='frm-order'>
+
+          <h1>Sus datos</h1>
+
+          <div className="name">
+            <label htmlFor="inp-name">Nombre</label>
+            <input id='inp-name' className='inp-name' type="text" placeholder='Nombre' required onChange={(e) => { setNombre(e.target.value); handleInputChange(e) }} />
+          </div>
+          <div className="name">
+
+            <label htmlFor="inp-mail">Email</label>
+            <input id='inp-mail' className='inp-mail' type="email" placeholder='Email' required onChange={(e) => { setEmail(e.target.value); handleInputChange(e) }} />
+          </div>
+
+          <button type="submit" className='btn-buy'>Enviar</button>
+
+        </form>
+      )}
+
+
+      {showForm ? '' :
+        <div className="buyout">
+
+          <img src="../src\assets\images\check.png" alt="Compra realiazada con exito." className='img-check' />
+          <h1>¡Gracias por tu compra!</h1>
+          <h4>Tu solicitud de compra fue recibida</h4>
+          <p>Tu pedido se encuentra en proceso de validación, en breve recibiras un correo con el detalle de tu compra.</p>
+
+          <h2>N° Orden de Compra: {orderId} </h2>
+
+        </div> }
+
+    </div>
+
   )
 }
 
--- src/components/SendOrders.jsx
+++ src/components/SendOrders.jsx
@@ -4,84 +4,58 @@ import { CartContext } from '../context/ShoppingCartContext'
 import { collection, addDoc, getFirestore } from 'firebase/firestore'
 import '../components/Cart/Cart.css'
 import { Link } from 'react-router-dom'
+import BuyOut from './BuyOut'
 
 const SendOrders = () => {
 
-    const { cart, clearCart, cartTotal } = useContext(CartContext)
+    const { cartTotal } = useContext(CartContext)
 
-    const [nombre, setNombre] = useState('')
-    const [email, setEmail] = useState('')
-    const [orderId, setOrderId] = useState(null)
-
-    const db = getFirestore()
-
-    const handleSubmit = (e) => {
-        e.preventDefault()
-
-        addDoc(ordersCollection, order).then(({ id }) =>
-            setOrderId(id))
-    }
-
-    const order = {
-        nombre,
-        email
-    }
-
-    const ordersCollection = collection(db, 'Orden')
+   
 
     return (
         <div>
-            <div className="cartitems-down">
-                <div className="cartitems-total">
-                    <h1>Total</h1>
-                    <div>
-                        <div className="cartitems-total-item">
-                            <p>Subtotal</p>
-                            <p>$  {cartTotal()} </p>
+
+                <div className="cartitems-down">
+                    <div className="cartitems-total">
+                        <h1>Total</h1>
+                        <div>
+                            <div className="cartitems-total-item">
+                                <p>Subtotal</p>
+                                <p>$  {cartTotal()} </p>
+                            </div>
+                            <hr />
+                            <div className="cartitems-total-item">
+                                <p>Envio</p>
+                                <p>Gratis</p>
+                            </div>
+                            <hr />
+                            <div className="cartitems-total-item">
+                                <h3>Total</h3>
+                                <h3>$  {cartTotal()}</h3>
+                            </div>
                         </div>
-                        <hr />
-                        <div className="cartitems-total-item">
-                            <p>Envio</p>
-                            <p>Gratis</p>
-                        </div>
-                        <hr />
-                        <div className="cartitems-total-item">
-                            <h3>Total</h3>
-                            <h3>$  {cartTotal()}</h3>
+                    </div>
+                    <div className="cartitems-promocode">
+                        <h3> | Si tienes un código de descuento, ingreselo aqui</h3>
+                        <div className="cartitems-promobox">
+
+                            <input className='inp-dsc' type="text" placeholder='Codigo de Promoción' />
+                            <button className='btn-dsc' >Aplicar Descuento</button>
+
+                            <Link to={"/buyout"}>
+                                <button className='btn-buy'>FINALIZAR COMPRA</button>
+                            </Link>
+
                         </div>
                     </div>
                 </div>
-                <div className="cartitems-promocode">
-                    <p> | Si tienes un código de descuento, ingreselo aqui</p>
-                    <div className="cartitems-promobox">
-                        <input className='inp-dsc' type="text" placeholder='Codigo de Promoción' />
-                        <button className='btn-dsc' >Aplicar Descuento</button>
 
-                        <form action="" className='frm-order'>
+            
+            {/* <div className='buy-container'>
 
-                            <h1>Sus datos</h1>
-                            <div className="name">
-                                <label htmlFor="inp-name">Nombre</label>
-                                <input id='inp-name' className='inp-name' type="text" placeholder='Nombre' onChange={(e) => setNombre(e.target.value)} />
-                            </div>
-                            <div className="name">
+                {orderId ? <BuyOut orden={orderId} /> : ''}
 
-                                <label htmlFor="inp-mail">Email</label>
-                                <input id='inp-mail' className='inp-mail' type="email" placeholder='Email' onChange={(e) => setEmail(e.target.value)} />
-                            </div>
-
-                            {/* <button type="submit">Enviar</button> */}
-                        </form>
-                        {/* <p>Su número de orden es: {orderId}</p> */}
-                    </div>
-                </div>
-            </div>
-
-            <div className='buy-container'>
-                <Link to={"/buyout"}>
-                    <button className='btn-buy'>PROCEDER A LA COMPRA</button>
-                </Link>
-            </div>
+            </div> */}
         </div>
     )
 }
